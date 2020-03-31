a268ba42a57131b437bbd7c3e2069faedf8382fc
Megan Christine McClure <mcclme02@luther.edu>
2020-03-31 14:04:59 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,38 +1,54 @@
-require 'pry'
-class School 
+# class School 
   
-  attr_reader :name, :roster
+#   attr_reader :name, :roster
   
-  def initialize(name)
-    @name = name 
-    @roster = {}
-  end 
+#   def initialize(name)
+#     @name = name
+#     @roster = {}
+#   end 
   
-  def roster
-    @roster
-  end 
+#   def roster
+#     @roster
+#   end 
   
-  def add_student(student, grade)
-    #binding.pry 
-    self.roster[grade] ||= []
-    #if self.roster[grade] already exists, leave it alone!!
-    #else set self.roster[grade] = [] =>this gets ride of the need for the if statemetn
-    self.roster[grade] << student 
-    # if roster[grade]
-    #   roster[grade] << student 
-    # else
-    #   roster[grade] = [student]
-    # end
-  end 
+#   def add_student(student, grade)
+#     self.roster[grade] ||= []
+#     self.roster[grade] << student
+#   end
   
-  def grade(grade)
-    self.roster[grade]
-  end 
+#   def grade(grade)
+#     self.roster[grade]
+#   end 
   
-  def sort 
-    self.roster.each do |grade, students|
-      students.sort!
-    end
-  end
+#   def sort
+#     self.roster.each do |grade, students|
+#       students.sort!
+#     end
+#   end
   
-end 
+# end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
